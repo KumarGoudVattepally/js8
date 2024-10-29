2d5b47a32e10946f3099b3c62271d7247d50ba4a
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-29 10:12:24 +0530

    as

---
+++ index.js
@@ -0,0 +1,10 @@
+var arr=[3,5,10,15,23,22,25,35,56];
+var arr1=[]
+for(i in arr){
+    if(arr[i]%5==0){
+        arr1[arr1.length]=arr[i]
+        // console.log(arr[i]);
+        
+    }
+}console.log(arr1);
+ var arr
